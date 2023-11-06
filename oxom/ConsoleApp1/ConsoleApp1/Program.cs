9f751a3427ab9827c2e7c08a36f8ce5a21266bc7
Mattias Gijsels <mattias.gijsels@hotmail.com>
2023-11-06 16:12:54 +0100

    rertrez

---
+++ oxom/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please give the number you want to display the sum of");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int num0,sum = 0;
+            for (num0=1;num0<=num;num0++)
+            {
+                sum += num0;
+            }
+            Console.WriteLine($"The sum of natural numbers up to term {num} = {sum}");
+        }  
+    }
+}
