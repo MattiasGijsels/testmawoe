df0eb02467b152026e9079df5f9715928e76319c
Mattias Gijsels <mattias.gijsels@hotmail.com>
2023-11-06 11:52:37 +0100

    testtttt

--- OEFENIIIINGEN/OEFENIIIINGEN/Program.cs
+++ OEFENIIIINGEN/OEFENIIIINGEN/Program.cs
@@ -135,7 +135,7 @@ namespace w3resources_oefeningen
 
             //Oefening2
 
-            Console.WriteLine("Enter your number");
+            /*Console.WriteLine("Enter your number");
             string txt = Console.ReadLine() ?? "";
 
             if (!double.TryParse(txt, out double x))
@@ -149,12 +149,30 @@ namespace w3resources_oefeningen
             else
             {
                 Console.WriteLine($"Your integer number is {txt}");
-            }
-
-            testesesese
+            }*/
 
 
 
+            float Pi = 3.14f;
+            Console.Write("Pi = ");
+            Console.WriteLine(Pi);
+            // Oef 2
+            decimal getal = 2500.25M;
+            Console.Write("getal = ");
+            Console.WriteLine(getal);
+            // Oef 3
+            int maxInt = int.MaxValue;
+            Console.Write("maxInt = ");
+            Console.WriteLine(maxInt);
+            Console.Write("maxInt + 1 = ");
+            Console.WriteLine(++maxInt);
+            // Oef 4
+            ushort maxUShort = ushort.MaxValue;
+            Console.Write("maxUShort = ");
+            Console.WriteLine(++maxUShort);
+            // Demo GIT
+            int a = 1;
+            Console.WriteLine(a++);
 
 
         }
---
+++ oefening method/oefening method/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace oefening_method
+{
+    internal class Program
+    {
+
+        /*static void Main(string[] args)
+        {
+            Sum();
+        }
+
+        private static void Sum()
+        {
+            Console.WriteLine("Please give the first number you would like to multiply");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please give the second number you would like to multiply");
+            double y = Convert.ToDouble(Console.ReadLine());
+            double result = (x + y);
+            Console.WriteLine("The Sum of the two numbers is " + result);
+
+        }*/
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please give in the sentence you would like the longest word from");
+            LangsteWoord();
+           
+        }
+
+        private static void LangsteWoord()
+        {
+            string line = Console.ReadLine() ?? "";
+            string[] words = line.Split(new[] { " " }, StringSplitOptions.None);
+            string word = "";
+            int ctr = 0;
+            foreach (String s in words)
+            {
+                if (s.Length > ctr)
+                {
+                    word = s;
+                    ctr = s.Length;
+                }
+            }
+
+            Console.WriteLine($"You wrote the followign sentence " + line + " and the longest word of that sentence is " + word);
+
+        }
+
+        /*private static void Counting()
+        {
+            bool inputnull = true;
+            int count;
+            string name = "";
+
+            while (inputnull)
+            {
+                Console.WriteLine("Please Give the sentence you would like to see the spaces count of");
+                name = Console.ReadLine() ?? "";
+
+                if (string.IsNullOrEmpty(name))
+                {
+                    Console.WriteLine("Your input is invalid, dingmush");
+                }
+                else
+                {
+                    inputnull = false;  
+                }
+            }
+            count = name.Split(' ').Length;
+            Console.WriteLine("The count is " + count);
+        }*/
+
+    }
+}
